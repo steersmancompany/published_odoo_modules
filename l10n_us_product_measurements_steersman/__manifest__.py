7d1a68a6f8df2ea6f9816d077685c4e2e8969a7d
Josh Fruits <josh.fruits@steersman.works>
2017-12-06 14:20:14 -0600

    Automated commit of l10n_us_product_measurements_steersman for publishing.

--- l10n_us_product_measurements_steersman/__manifest__.py
+++ l10n_us_product_measurements_steersman/__manifest__.py
@@ -3,16 +3,20 @@
 	'name': 'US/Imperial Product Measurements',
 	'summary': 'Maintain product weight, volume and dimensions in any UoM',
 	'category': 'Localization',
-	'version': '0.0.4',
+	'version': '0.0.6',
 	'author': 'Steersman Company',
 	'website': 'https://steersman.works',
-	'depends': ['stock'],
+	'depends': [
+		'stock'
+	],
 	'data': [
-        'views/product_template_views.xml',
+		'views/product_template_views.xml',
 		'views/product_views.xml',
 		'data/product_data.xml'
-    ],
-    'images': ['static/description/banner.png'],
+	],
+	'images': [
+		'static/description/banner.png'
+	],
 	'application': False,
 	'installable': True,
 	'auto_install': True,
--- l10n_us_product_measurements_steersman/models/__init__.py
+++ l10n_us_product_measurements_steersman/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-import product
-import product_template
+from . import product
+from . import product_template
